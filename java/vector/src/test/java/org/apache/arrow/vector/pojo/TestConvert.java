12711b2d1f7fe801c393f11b40ec4b45a2dc58a5
Steven Phillips <steven@dremio.com>
2016-08-15 17:48:20 -0700

    ARROW-259: Add Schema pojo

--- java/vector/src/test/java/org/apache/arrow/vector/pojo/TestConvert.java
+++ java/vector/src/test/java/org/apache/arrow/vector/pojo/TestConvert.java
@@ -24,6 +24,7 @@ import org.apache.arrow.vector.types.pojo.ArrowType.Int;
 import org.apache.arrow.vector.types.pojo.ArrowType.Tuple;
 import org.apache.arrow.vector.types.pojo.ArrowType.Utf8;
 import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
 import org.junit.Test;
 
 import java.util.List;
@@ -51,11 +52,29 @@ public class TestConvert {
     run(initialField);
   }
 
+  @Test
+  public void schema() {
+    ImmutableList.Builder<Field> childrenBuilder = ImmutableList.builder();
+    childrenBuilder.add(new Field("child1", true, Utf8.INSTANCE, null));
+    childrenBuilder.add(new Field("child2", true, new FloatingPoint(0), ImmutableList.<Field>of()));
+    Schema initialSchema = new Schema(childrenBuilder.build());
+    run(initialSchema);
+
+  }
+
   private void run(Field initialField) {
     FlatBufferBuilder builder = new FlatBufferBuilder();
     builder.finish(initialField.getField(builder));
     org.apache.arrow.flatbuf.Field flatBufField = org.apache.arrow.flatbuf.Field.getRootAsField(builder.dataBuffer());
     Field finalField = Field.convertField(flatBufField);
     assertEquals(initialField, finalField);
+  }
+
+  private void run(Schema initialSchema) {
+    FlatBufferBuilder builder = new FlatBufferBuilder();
+    builder.finish(initialSchema.getSchema(builder));
+    org.apache.arrow.flatbuf.Schema flatBufSchema = org.apache.arrow.flatbuf.Schema.getRootAsSchema(builder.dataBuffer());
+    Schema finalSchema = Schema.convertSchema(flatBufSchema);
+    assertEquals(initialSchema, finalSchema);
   }
 }
